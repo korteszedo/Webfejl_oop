4cce04faa25f8a794f471ca95b8bc35334a02821
Toth Balint <toth.balint@BOLYAI.LOCAL>
2024-11-29 12:48:16 +0100

    fjhfbbdsfjbdbfur

--- basics/main.js
+++ basics/main.js
@@ -1,14 +1,37 @@
 alert("buzi alex");
 
 
-
-
-function Player(nickname){
+function Player(nickname, playedmatch){
     this.nickname = nickname;
+    this.playedmatch = playedmatch;
     
 }
 
 
-const gombszab = new Player("gombszab");
+Player.prototype.play = function(){
+    this.playedmatch ++;
+    
+    console.log(this.nickname, this.playedmatch)
+}
 
-console.log(gombszab);
+
+Player.prototype.getTrierlvl = function(){
+    if(this.playedmatch <= 3){
+        return "A tier"
+    }
+    if (this.playedmatch > 3 && this.playedmatch < 6) {
+        return "B tier"
+    } else {
+        return "C tier"
+    }
+}
+
+const gombszab = new Player("gombszab", 1);
+gombszab.play()
+gombszab.play()
+gombszab.play()
+
+console.log(gombszab.getTrierlvl());
+
+
+
