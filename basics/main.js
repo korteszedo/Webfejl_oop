61ae0ed69a9daccfd45939706453a2cf125cf196
Toth Balint <toth.balint@BOLYAI.LOCAL>
2024-11-29 12:56:04 +0100

    fjhfbbdsfjbdbfurdsadsada

--- basics/main.js
+++ basics/main.js
@@ -1,37 +1,46 @@
 alert("buzi alex");
 
 
-function Player(nickname, playedmatch){
-    this.nickname = nickname;
-    this.playedmatch = playedmatch;
+// function Player(nickname, playedmatch){
+//     this.nickname = nickname;
+//     this.playedmatch = playedmatch;
     
-}
+// }
 
 
-Player.prototype.play = function(){
-    this.playedmatch ++;
-    
-    console.log(this.nickname, this.playedmatch)
-}
 
 
-Player.prototype.getTrierlvl = function(){
-    if(this.playedmatch <= 3){
-        return "A tier"
-    }
-    if (this.playedmatch > 3 && this.playedmatch < 6) {
-        return "B tier"
-    } else {
-        return "C tier"
-    }
-}
-
-const gombszab = new Player("gombszab", 1);
+// const gombszab = new Player("gombszab", 1);
 gombszab.play()
 gombszab.play()
 gombszab.play()
 
 console.log(gombszab.getTrierlvl());
+
+class Player{
+    constructor(nickname, playedmatch){
+        this.nickname = nickname;
+        this.playedmatch = playedmatch;
+    }
+    play(){
+        
+    this.playedmatch ++;
+    
+    console.log(this.nickname, this.playedmatch)
+    }
+
+
+    getTrierlvl(){
+     if(this.playedmatch <= 3){
+         return "A tier"
+     }
+        if (this.playedmatch > 3 && this.playedmatch < 6) {
+            return "B tier"
+     } else {
+            return "C tier"
+        }
+    }
+}
 
 
 
