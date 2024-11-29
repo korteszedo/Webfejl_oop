5666981aea59752b6c90c14e1184ae19ae16d0a6
Toth Balint <toth.balint@BOLYAI.LOCAL>
2024-11-29 11:42:46 +0100

    Oláh Áron Dániel egész orán játszott de nem mertem szólni, mert megfenyegetett hogy körbe focizik

--- basics/main.js
+++ basics/main.js
@@ -1,1 +1,14 @@
-alert("buzi alex")
+alert("buzi alex");
+
+
+
+
+function Player(nickname){
+    this.nickname = nickname;
+    
+}
+
+
+const gombszab = new Player("gombszab");
+
+console.log(gombszab);
